dbafb8323c57e8a087ec5ac9684e048bf27563e4
DYNOV <dynovember05@naver.com>
2025-01-03 01:45:13 +0900

    building setup
    초기 설정

---
+++ tuk-dor/src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom/client";
+
+const App= () => {
+  return <h1>안녕하세요!</h1>;  
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App/>); 
